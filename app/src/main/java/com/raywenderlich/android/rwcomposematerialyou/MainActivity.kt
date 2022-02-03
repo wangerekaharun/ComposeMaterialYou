14e387bfb75f660e83c6d2156ffdce26b2548d3b
wangerekaharun <harunwangereka@gmail.com>
2022-02-03 23:27:20 +0300

    setting project in compose

--- app/src/main/java/com/raywenderlich/android/rwcomposematerialyou/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/rwcomposematerialyou/MainActivity.kt
@@ -35,22 +35,42 @@
 package com.raywenderlich.android.rwcomposematerialyou
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.raywenderlich.android.rwcomposematerialyou.ui.theme.ComposeMaterialYou
 
-/**
- * Main Screen
- */
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
-    // Switch to AppTheme for displaying the activity
-    setTheme(R.style.AppTheme)
-
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    setContent {
+      ComposeMaterialYou {
+        Surface(
+          modifier = Modifier.fillMaxSize(),
+          color = MaterialTheme.colors.background
+        ) {
+          Greeting("Android")
+        }
+      }
+    }
+  }
+}
 
-    // Your code
+@Composable
+fun Greeting(name: String) {
+  Text(text = "Hello $name!")
+}
 
-
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+  ComposeMaterialYou {
+    Greeting("Android")
   }
 }
---
+++ app/src/main/java/com/raywenderlich/android/rwcomposematerialyou/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.raywenderlich.android.rwcomposematerialyou.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val ColorPrimary = Color(0xFF006837)
+val ColorPrimaryDark = Color(0xFF004012)
+val ColorAccent = Color(0xFFc75f00)
