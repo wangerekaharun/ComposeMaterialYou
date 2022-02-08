4750c1fbc1dbf3c6122d5f03e977cb7de039e5d8
wangerekaharun <harunwangereka@gmail.com>
2022-02-08 14:23:02 +0300

    setup koin for DI

--- app/src/main/java/com/raywenderlich/android/rwcomposematerialyou/ui/data/repository/EventsRepository.kt
+++ app/src/main/java/com/raywenderlich/android/rwcomposematerialyou/ui/data/repository/EventsRepository.kt
@@ -1,17 +1,23 @@
 package com.raywenderlich.android.rwcomposematerialyou.ui.data.repository
 
+import com.raywenderlich.android.rwcomposematerialyou.ui.data.AppDataBase
+import com.raywenderlich.android.rwcomposematerialyou.ui.data.models.Events
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
 interface EventsRepository {
-  suspend fun addEvent()
-  suspend fun getAllEvents()
+  suspend fun addEvent(events: Events)
+  suspend fun getAllEvents(): Flow<List<Events>>
 }
 
-class EventsRepositoryImpl: EventsRepository {
-  override suspend fun addEvent() {
-    TODO("Not yet implemented")
+class EventsRepositoryImpl(private val appDatabase: AppDataBase): EventsRepository {
+  override suspend fun addEvent(events: Events) {
+   appDatabase.eventsDao().createEvent(events)
   }
 
-  override suspend fun getAllEvents() {
-    TODO("Not yet implemented")
-  }
+  override suspend fun getAllEvents():Flow<List<Events>> =
+    flow {
+      appDatabase.eventsDao().getAllEvents()
+    }
 
 }
