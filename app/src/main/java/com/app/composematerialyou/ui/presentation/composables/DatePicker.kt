b5a2fd467bd09e93d34f6faa37480706e5e61fa0
Harun Wangereka <harunwangereka@gmail.com>
2022-03-15 00:20:56 +0300

    remove rw branding on starter project

---
+++ app/src/main/java/com/app/composematerialyou/ui/presentation/composables/DatePicker.kt
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2022 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * This project and source code may use libraries or frameworks that are
+ * released under various Open-Source licenses. Use of those libraries and
+ * frameworks are governed by their own individual licenses.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.app.composematerialyou.ui.presentation.composables
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension.Companion.fillToConstraints
+import com.app.composematerialyou.ui.presentation.viewmodels.EventsViewModel
+import com.app.composematerialyou.ui.utils.showDatePicker
+
+@Composable
+fun DatePicker(
+  datePicked: String?,
+  updatedDate: (date: String?) -> Unit,
+  activity: AppCompatActivity,
+  eventsViewModel: EventsViewModel
+) {
+  val focusManager = LocalFocusManager.current
+  Box(
+    modifier = Modifier
+      .fillMaxWidth()
+      .wrapContentSize(Alignment.TopStart)
+      .padding(10.dp)
+      .background(color = Color.White, shape = RoundedCornerShape(10.dp))
+      .clickable(
+        indication = rememberRipple(bounded = true),
+        interactionSource = remember {
+          MutableInteractionSource()
+        }
+      ) {
+        focusManager.clearFocus()
+        showDatePicker(
+          activity,
+          eventsViewModel.date,
+          updatedDate
+        )
+      }
+  ) {
+    eventsViewModel.date = datePicked.toString()
+
+    ConstraintLayout(
+      modifier = Modifier
+        .fillMaxWidth()
+        .padding(16.dp)
+    ) {
+
+      val (label, iconView) = createRefs()
+
+      val showLabel = if (datePicked.isNullOrEmpty()) "Choose a Date" else datePicked
+
+      Text(
+        text = showLabel,
+        color = MaterialTheme.colorScheme.onSurface,
+        modifier = Modifier
+          .fillMaxWidth()
+          .constrainAs(label) {
+            top.linkTo(parent.top)
+            bottom.linkTo(parent.bottom)
+            start.linkTo(parent.start)
+            end.linkTo(iconView.start)
+            width = fillToConstraints
+          }
+      )
+
+      Icon(
+        imageVector = Icons.Default.DateRange,
+        contentDescription = null,
+        modifier = Modifier
+          .size(20.dp, 20.dp)
+          .constrainAs(iconView) {
+            end.linkTo(parent.end)
+            top.linkTo(parent.top)
+            bottom.linkTo(parent.bottom)
+          },
+        tint = MaterialTheme.colorScheme.onSurface
+      )
+
+    }
+
+  }
+}
