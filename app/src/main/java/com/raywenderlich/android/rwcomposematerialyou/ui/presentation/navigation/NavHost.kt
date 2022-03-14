8a37f3af84553f11fe0c46510d2e50b084e16be3
Harun Wangereka <harunwangereka@gmail.com>
2022-03-15 00:00:56 +0300

    NvaHost For Starter Project Ready

--- app/src/main/java/com/raywenderlich/android/rwcomposematerialyou/ui/presentation/navigation/NavHost.kt
+++ app/src/main/java/com/raywenderlich/android/rwcomposematerialyou/ui/presentation/navigation/NavHost.kt
@@ -33,94 +33,29 @@
  */
 package com.raywenderlich.android.rwcomposematerialyou.ui.presentation.navigation
 
-import androidx.compose.animation.AnimatedContentScope
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.expandIn
-import androidx.compose.animation.shrinkOut
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.navigation.NavHostController
-import com.google.accompanist.navigation.animation.AnimatedNavHost
-import com.google.accompanist.navigation.animation.composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import com.raywenderlich.android.rwcomposematerialyou.ui.presentation.screens.EventScreen
 import com.raywenderlich.android.rwcomposematerialyou.ui.presentation.screens.HomeScreen
 import com.raywenderlich.android.rwcomposematerialyou.ui.presentation.viewmodels.EventsViewModel
 
-@ExperimentalAnimationApi
+
 @ExperimentalComposeUiApi
 @ExperimentalMaterial3Api
 @Composable
 fun AppNavigation(navController: NavHostController, eventsViewModel: EventsViewModel) {
-  AnimatedNavHost(
+  NavHost(
     navController = navController,
     startDestination = Screens.HomeScreen.route,
-    enterTransition = { expandIn(animationSpec = tween(800)) },
-    exitTransition = { shrinkOut(animationSpec = tween(800)) }
   ) {
-    composable(Screens.HomeScreen.route,
-      enterTransition = {
-        if (initialState.destination.route == Screens.EventInputScreen.route) slideIntoContainer(
-          AnimatedContentScope.SlideDirection.Left,
-          animationSpec = tween(600)
-        )
-        else null
-      },
-      exitTransition = {
-        if (targetState.destination.route == Screens.EventInputScreen.route) slideOutOfContainer(
-          AnimatedContentScope.SlideDirection.Left,
-          animationSpec = tween(600)
-        )
-        else null
-      },
-      popEnterTransition = {
-        if (initialState.destination.route == Screens.EventInputScreen.route) slideIntoContainer(
-          AnimatedContentScope.SlideDirection.Right,
-          animationSpec = tween(600)
-        )
-        else null
-      },
-      popExitTransition = {
-        if (targetState.destination.route == Screens.EventInputScreen.route) slideOutOfContainer(
-          AnimatedContentScope.SlideDirection.Right,
-          animationSpec = tween(600)
-        )
-        else null
-      }
-    ) {
+    composable(Screens.HomeScreen.route) {
       HomeScreen(navController, eventsViewModel)
     }
-    composable(Screens.EventInputScreen.route,
-      enterTransition = {
-        if (initialState.destination.route == Screens.HomeScreen.route) slideIntoContainer(
-          AnimatedContentScope.SlideDirection.Left,
-          animationSpec = tween(600)
-        )
-        else null
-      },
-      exitTransition = {
-        if (targetState.destination.route == Screens.HomeScreen.route) slideOutOfContainer(
-          AnimatedContentScope.SlideDirection.Left,
-          animationSpec = tween(600)
-        )
-        else null
-      },
-      popEnterTransition = {
-        if (initialState.destination.route == Screens.HomeScreen.route) slideIntoContainer(
-          AnimatedContentScope.SlideDirection.Right,
-          animationSpec = tween(600)
-        )
-        else null
-      },
-      popExitTransition = {
-        if (targetState.destination.route == Screens.HomeScreen.route) slideOutOfContainer(
-          AnimatedContentScope.SlideDirection.Right,
-          animationSpec = tween(600)
-        )
-        else null
-      }
-    ) {
+    composable(Screens.EventInputScreen.route) {
       EventScreen(navController, eventsViewModel)
     }
   }
